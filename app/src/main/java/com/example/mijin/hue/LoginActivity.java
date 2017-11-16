aded56b6030a7367a79c492e69a3b4734120ea5a
Bobgoodd <30946850+Bobgoodd@users.noreply.github.com>
2017-11-16 17:34:40 +0900

    로그인화면 로그인탭-프로젝트목록 프로젝트탭-회의방,문서목록,문서목록-문서내용,업무목록 ++++++++++++++++++++++++++++++++++++++++++++++++++++ 로그인탭-친구목록(친구목록 띄우기,액티비티-액티비티-프래그먼트 데이터 전달) ++++++++++++++++++++++++++++++++++++++++++++++++++++ 로그인탭-프로젝트목록 추가 진행해야 할 사항-php코드, 데이터베이스 쿼리 ++++++++++++++++++++++++++++++++++++++++++++++++++++ 로그인탭-프로젝트 추가, 친구 추가 데이터베이스 연동 진행해야 할 사항-프로젝트 삭제, 수정 버튼 이벤트 ++++++++++++++++++++++++++++++++++++++++++++++++++++ 스플래시,로그인화면UI변경,ID/PASSWORD찾기 진행해야 할 사항-MYSQL한글 깨짐 현상 해결, 일정부분 연결 ++++++++++++++++++++++++++++++++++++++++++++++++++++ 프로젝트 삭제, 수정 버튼 기능 진행해야 할 사항-MYSQL 한글 깨짐 현상 해결, 일정부분 연결 ++++++++++++++++++++++++++++++++++++++++++++++++++++ 프로젝트 탭2- 문서수정기능 진행해야 할 사항-MYSQL 한글 깨짐 현상 해결, 일정부분 연결 ++++++++++++++++++++++++++++++++++++++++++++++++++++ 일정부분 UI 진행해야 할 사항-MYSQL 한글 깨짐 현상 해결, 일정부분 연결 ++++++++++++++++++++++++++++++++++++++++++++++++++++ 로그인 수정

--- app/src/main/java/com/example/mijin/hue/LoginActivity.java
+++ app/src/main/java/com/example/mijin/hue/LoginActivity.java
@@ -1,7 +1,11 @@
 package com.example.mijin.hue;
 
 import android.app.Activity;
+import android.content.ContentValues;
+import android.content.DialogInterface;
 import android.content.Intent;
+import android.content.SharedPreferences;
+import android.os.AsyncTask;
 import android.os.Bundle;
 import android.support.v7.app.AlertDialog;
 import android.view.View;
@@ -9,9 +13,6 @@ import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Toast;
 
-import com.android.volley.RequestQueue;
-import com.android.volley.Response;
-import com.android.volley.toolbox.Volley;
 import com.example.mijin.hue.LoginTab.LoginTabActivity;
 import com.facebook.CallbackManager;
 import com.facebook.FacebookCallback;
@@ -19,8 +20,6 @@ import com.facebook.FacebookException;
 import com.facebook.FacebookSdk;
 import com.facebook.login.LoginResult;
 import com.facebook.login.widget.LoginButton;
-
-import org.json.JSONObject;
 
 public class LoginActivity extends Activity {
 
@@ -50,29 +49,32 @@ public class LoginActivity extends Activity {
         } );
         login = (Button) findViewById(R.id.login);
 
- /*       login.setOnClickListener(new Button.OnClickListener(){
-            @Override
-            public void onClick(View view) {
-                if(idText.getText().toString()==null||passwordText.getText().toString()==null){
-                    AlertDialog.Builder builder = new AlertDialog.Builder(view.getContext());
-                    builder.setMessage("로그인 정보를 입력해주세요.");
-                    builder.setPositiveButton("확인",new DialogInterface.OnClickListener(){
-                        @Override
-                        public void onClick(DialogInterface dialogInterface, int i) {
+        login.setOnClickListener(new Button.OnClickListener() {
+                                     @Override
+                                     public void onClick(View view) {
+                                         if (idText.getText().toString() == null || passwordText.getText().toString() == null) {
+                                             AlertDialog.Builder builder = new AlertDialog.Builder(view.getContext());
+                                             builder.setMessage("로그인 정보를 입력해주세요.");
+                                             builder.setPositiveButton("확인", new DialogInterface.OnClickListener() {
+                                                 @Override
+                                                 public void onClick(DialogInterface dialogInterface, int i) {
 
-                        }
-                    });
-                    builder.show();
-                }else {
-                    SharedPreferences prefs = getSharedPreferences("PrefName", MODE_PRIVATE);
-                    SharedPreferences.Editor editor = prefs.edit();
-                    editor.putString("id", idText.getText().toString());
-                    editor.putString("pw", passwordText.getText().toString());
-                    editor.commit();
+                                                 }
+                                             });
+                                             builder.show();
+                                         } else {
 
-                    Intent intent = new Intent(LoginActivity.this, LoginTabActivity.class);
-                    startActivity(intent);
-                }*/
+                                             String url = "http://uoshue.dothome.co.kr/loginK.php";
+                                             ContentValues values = new ContentValues();
+                                             values.put("id",idText.getText().toString());
+                                             values.put("pw",passwordText.getText().toString());
+
+                                             NetworkTask14 networkTask14 = new NetworkTask14(url, values);
+                                             networkTask14.execute();
+
+                                         }
+                                     }
+                                 });
 //                final String userId = idText.getText().toString();
 //                final String userPassword = passwordText.getText().toString();
 //                Response.Listener<String> responseListener = new Response.Listener<String>(){
@@ -109,7 +111,7 @@ public class LoginActivity extends Activity {
 
    /*         }
         });*/
-
+/*
         login.setOnClickListener(new View.OnClickListener(){
 
             @Override
@@ -122,21 +124,27 @@ public class LoginActivity extends Activity {
                     @Override
                     public void onResponse(String response) {
                         try{
-                            JSONObject jsonResponse = new JSONObject(response);
-                            boolean success = jsonResponse.getBoolean("success");
-                            if(success){
-                                String userId = jsonResponse.getString("userId");
-                                String userPassword = jsonResponse.getString("userPassword");
-                                Intent intent = new Intent(LoginActivity.this, LoginTabActivity.class);
-                                intent.putExtra("userId", userId);
-                                intent.putExtra("userPassword", userPassword);
-                                startActivity(intent);
-                            }else{
-                                AlertDialog.Builder builder = new AlertDialog.Builder(LoginActivity.this);
-                                builder.setMessage("로그인에 실패했습니다.").
-                                        setNegativeButton("다시 시도", null)
-                                        .create()
-                                        .show();
+                            if(response!=null){
+                                    JSONArray jarr = new JSONArray(response);
+                            for(int i=0;i<jarr.length();i++) {
+                                JSONObject jsonResponse = (JSONObject) jarr.get(i);
+
+                               // boolean success = jsonResponse.getBoolean("success");
+                                //if (success) {
+                                    String userId = jsonResponse.getString("userId");
+                                    String userPassword = jsonResponse.getString("userPassword");
+                                    Intent intent = new Intent(LoginActivity.this, LoginTabActivity.class);
+                                    intent.putExtra("userId", userId);
+                                    intent.putExtra("userPassword", userPassword);
+                                    startActivity(intent);
+                                }
+                                if(jarr.length()==0) {
+                                    AlertDialog.Builder builder = new AlertDialog.Builder(LoginActivity.this);
+                                    builder.setMessage("로그인에 실패했습니다.").
+                                            setNegativeButton("다시 시도", null)
+                                            .create()
+                                            .show();
+                                }
                             }
                         }catch(Exception e){
                             e.printStackTrace();
@@ -149,37 +157,38 @@ public class LoginActivity extends Activity {
                 queue.add(loginRequest);
             }
         });
-        FacebookSdk.sdkInitialize(getApplicationContext());
+        */
+                FacebookSdk.sdkInitialize(getApplicationContext());
 
-        callbackManager = CallbackManager.Factory.create();
-        LoginButton loginButton = (LoginButton)findViewById(R.id.login_buttonF);
-        loginButton.registerCallback(callbackManager, new FacebookCallback<LoginResult>() {
-            @Override
-            public void onSuccess(LoginResult loginResult) {
-                Toast.makeText(getApplicationContext(),"로그인 성공",Toast.LENGTH_LONG).show();
-            }
+                callbackManager = CallbackManager.Factory.create();
+                LoginButton loginButton = (LoginButton) findViewById(R.id.login_buttonF);
+                loginButton.registerCallback(callbackManager, new FacebookCallback<LoginResult>() {
+                    @Override
+                    public void onSuccess(LoginResult loginResult) {
+                        Toast.makeText(getApplicationContext(), "로그인 성공", Toast.LENGTH_LONG).show();
+                    }
 
-            @Override
-            public void onCancel() {
-                Toast.makeText(getApplicationContext(), "로그인 취소", Toast.LENGTH_LONG).show();
-            }
+                    @Override
+                    public void onCancel() {
+                        Toast.makeText(getApplicationContext(), "로그인 취소", Toast.LENGTH_LONG).show();
+                    }
 
-            @Override
-            public void onError(FacebookException error) {
-                Toast.makeText(getApplicationContext(), "로그인 에러", Toast.LENGTH_LONG).show();
-            }
-        });
+                    @Override
+                    public void onError(FacebookException error) {
+                        Toast.makeText(getApplicationContext(), "로그인 에러", Toast.LENGTH_LONG).show();
+                    }
+                });
 //
-        Button find = (Button) findViewById(R.id.find);
-        find.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Intent intent = new Intent(getApplicationContext(), FindInformationActivity.class);
-                startActivity(intent);
-            }
-        });
+                Button find = (Button) findViewById(R.id.find);
+                find.setOnClickListener(new View.OnClickListener() {
+                    @Override
+                    public void onClick(View view) {
+                        Intent intent = new Intent(getApplicationContext(), FindInformationActivity.class);
+                        startActivity(intent);
+                    }
+                });
 
-    }
+            }
 
     @Override
     protected void onActivityResult(int requestCode, int resultCode, Intent data){
@@ -191,4 +200,61 @@ public class LoginActivity extends Activity {
         }
         callbackManager.onActivityResult(requestCode, resultCode, data);
     }
+
+    public class NetworkTask14 extends AsyncTask<Void, Void, String> {
+
+        private String url;
+        private ContentValues values;
+
+        public NetworkTask14(String url, ContentValues values) {
+
+            this.url = url;
+            this.values = values;
+        }
+
+        @Override
+        protected String doInBackground(Void... params) {
+
+            String result;
+            RequestHttpURLConnection requestHttpURLConnection = new RequestHttpURLConnection();
+            result = requestHttpURLConnection.request(url, values);
+
+            return result;
+
+        }
+
+        @Override
+        protected void onPostExecute(String result) {
+            super.onPostExecute(result);
+
+
+            if(result!=null&&!result.equals("")) {
+                if(result.equals("success")){
+                    SharedPreferences prefs = getSharedPreferences("PrefName", MODE_PRIVATE);
+                    SharedPreferences.Editor editor = prefs.edit();
+                    editor.putString("id", idText.getText().toString());
+                    editor.putString("pw", passwordText.getText().toString());
+                    editor.commit();
+
+                    Intent intent = new Intent(LoginActivity.this, LoginTabActivity.class);
+                    startActivity(intent);
+                }else{
+                    AlertDialog.Builder builder = new AlertDialog.Builder(LoginActivity.this);
+                    builder.setMessage("로그인 정보가 다릅니다.");
+                    builder.setPositiveButton("확인", new DialogInterface.OnClickListener() {
+                        @Override
+                        public void onClick(DialogInterface dialogInterface, int i) {
+
+                        }
+                    });
+                    builder.show();
+                }
+            }
+
+
+
+        }
+    }
+
 }
+
