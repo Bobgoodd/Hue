a6ed7acf755e77e2c11f7a9ba34746f917ea1210
Bobgoodd <30946850+Bobgoodd@users.noreply.github.com>
2017-11-24 10:19:33 +0900

    로그인화면 로그인탭-프로젝트목록 프로젝트탭-회의방,문서목록,문서목록-문서내용,업무목록 ++++++++++++++++++++++++++++++++++++++++++++++++++++ 로그인탭-친구목록(친구목록 띄우기,액티비티-액티비티-프래그먼트 데이터 전달) ++++++++++++++++++++++++++++++++++++++++++++++++++++ 로그인탭-프로젝트목록 추가 진행해야 할 사항-php코드, 데이터베이스 쿼리 ++++++++++++++++++++++++++++++++++++++++++++++++++++ 로그인탭-프로젝트 추가, 친구 추가 데이터베이스 연동 진행해야 할 사항-프로젝트 삭제, 수정 버튼 이벤트 ++++++++++++++++++++++++++++++++++++++++++++++++++++ 스플래시,로그인화면UI변경,ID/PASSWORD찾기 진행해야 할 사항-MYSQL한글 깨짐 현상 해결, 일정부분 연결 ++++++++++++++++++++++++++++++++++++++++++++++++++++ 프로젝트 삭제, 수정 버튼 기능 진행해야 할 사항-MYSQL 한글 깨짐 현상 해결, 일정부분 연결 ++++++++++++++++++++++++++++++++++++++++++++++++++++ 프로젝트 탭2- 문서수정기능 진행해야 할 사항-MYSQL 한글 깨짐 현상 해결, 일정부분 연결 ++++++++++++++++++++++++++++++++++++++++++++++++++++ 일정부분 UI 진행해야 할 사항-MYSQL 한글 깨짐 현상 해결, 일정부분 연결 ++++++++++++++++++++++++++++++++++++++++++++++++++++ 로그인 수정 ++++++++++++++++++++++++++++++++++++++++++++++++++++ UI-1 ++++++++++++++++++++++++++++++++++++++++++++++++++++ 탭레이아웃수정전 ++++++++++++++++++++++++++++++++++++++++++++++++++++ 탭레이아웃수정

---
+++ app/src/main/java/com/example/mijin/hue/MainActivity.java
@@ -0,0 +1,147 @@
+package com.example.mijin.hue;
+
+import android.content.SharedPreferences;
+import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.design.widget.NavigationView;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentTransaction;
+import android.support.v4.view.GravityCompat;
+import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.ActionBar;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.MenuItem;
+import android.view.View;
+
+import com.example.mijin.hue.LoginTab.Tab1.LoginTabFragment1;
+import com.example.mijin.hue.LoginTab.Tab2.LoginTabFragment2;
+import com.example.mijin.hue.LoginTab.Tab3.LoginTabFragment3;
+
+/**
+ * Created by mijin on 2017-11-23.
+ */
+
+
+public class MainActivity extends AppCompatActivity implements NavigationView.OnNavigationItemSelectedListener, DrawerLayout.DrawerListener {
+
+    DrawerLayout drawerLayout;
+    android.support.v4.app.Fragment fragment;
+    Toolbar toolbar;
+   // TabLayout tabLayout;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        SharedPreferences prefs = getSharedPreferences("PrefName",MODE_PRIVATE);
+        SharedPreferences.Editor editor = prefs.edit();
+        editor.putString("id","mijin054");
+
+        setupToolbar();
+        setupNavigationView();
+       // setupTablayout();
+
+        fragment = new MainFragment();
+        FragmentManager fragmentManager = getSupportFragmentManager();
+        FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
+        fragmentTransaction.add(R.id.frame, fragment);
+        fragmentTransaction.commit();
+
+
+
+    }
+
+
+    private void setupNavigationView(){
+
+        drawerLayout = (DrawerLayout) findViewById(R.id.drawer_layout);
+        drawerLayout.setDrawerListener(this);
+        NavigationView navigationView = (NavigationView) findViewById(R.id.navigation);
+        navigationView.setNavigationItemSelectedListener(this);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case android.R.id.home:
+                drawerLayout.openDrawer(GravityCompat.START);
+                return true;
+        }
+        return super.onOptionsItemSelected(item);
+    }
+
+    @Override
+    public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+
+        switch(item.getItemId()) {
+            case R.id.main:
+                fragment = new MainFragment();
+                break;
+            case R.id.navigation_item_1:
+                fragment = new LoginTabFragment1();
+                break;
+            case R.id.navigation_item_2:
+                fragment = new LoginTabFragment2();
+                break;
+            case R.id.navigation_item_3:
+                fragment = new LoginTabFragment3();
+                break;
+        }
+
+        FragmentManager fragmentManager = getSupportFragmentManager();
+        FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
+        fragmentTransaction.replace(R.id.frame, fragment);
+        fragmentTransaction.commit();
+
+        return true;
+
+    }
+
+    private void setupToolbar(){
+        toolbar = (Toolbar) findViewById(R.id.toolbar2);
+        setSupportActionBar(toolbar);
+        // Show menu icon
+        final ActionBar ab = getSupportActionBar();
+        ab.setHomeAsUpIndicator(R.drawable.ic_menu);
+        ab.setDisplayHomeAsUpEnabled(true);
+    }
+
+
+    @Override
+    public void onDrawerSlide(View drawerView, float slideOffset) {
+
+    }
+
+    @Override
+    public void onDrawerOpened(View drawerView) {
+        getSupportActionBar().setHomeAsUpIndicator(R.mipmap.ic_launcher_round);
+    }
+
+    @Override
+    public void onDrawerClosed(View drawerView) {
+        getSupportActionBar().setHomeAsUpIndicator(R.mipmap.ic_launcher);
+
+
+    }
+
+    @Override
+    public void onDrawerStateChanged(int newState) {
+
+    }
+
+/*
+    private void setupTablayout(){
+
+        tabLayout = (TabLayout) findViewById(tabLayout);
+        tabLayout.setTabGravity(TabLayout.GRAVITY_FILL);
+        tabLayout.addTab(tabLayout.newTab().setText("Tab 1"));
+        tabLayout.addTab(tabLayout.newTab().setText("Tab 2"));
+        tabLayout.addTab(tabLayout.newTab().setText("Tab 3"));
+        tabLayout.addTab(tabLayout.newTab().setText("Tab 4"));
+    }
+  */
+}
+
